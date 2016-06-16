36953c60fbd7b8678ddd8d91caae1fda60cd978a
Christian <cwunderlich@almandor.de>
2016-06-16 11:18:28 +0200

    Kleinere Anpassungen

--- doc/html/files.js
+++ doc/html/files.js
@@ -1,20 +1,4 @@
 var files =
 [
-    [ "Backpack.py", "Backpack_8py.html", null ],
-    [ "calc_ep.py", "calc__ep_8py.html", "calc__ep_8py" ],
-    [ "confbox.py", "confbox_8py.html", "confbox_8py" ],
-    [ "epcalcdefs.py", "epcalcdefs_8py.html", "epcalcdefs_8py" ],
-    [ "errbox.py", "errbox_8py.html", [
-      [ "raisedError", "classrpgtoolbox_1_1errbox_1_1raisedError.html", null ]
-    ] ],
-    [ "globaltools.py", "globaltools_8py.html", "globaltools_8py" ],
-    [ "helptools.py", "helptools_8py.html", "helptools_8py" ],
-    [ "itemshop.py", "itemshop_8py.html", null ],
-    [ "logbox.py", "logbox_8py.html", "logbox_8py" ],
-    [ "MERSTables.py", "MERSTables_8py.html", [
-      [ "Magic", "classrpgToolDefinitions_1_1MERSTables_1_1Magic.html", null ]
-    ] ],
-    [ "rpgtools.py", "rpgtools_8py.html", "rpgtools_8py" ],
-    [ "window.py", "window_8py.html", "window_8py" ],
-    [ "winhelper.py", "winhelper_8py.html", "winhelper_8py" ]
+    [ "rucksack.py", "rucksack_8py.html", null ]
 ];
--- doc/html/navtreedata.js
+++ doc/html/navtreedata.js
@@ -1,24 +1,7 @@
 var NAVTREE =
 [
-  [ "rpg tools (Python)", "index.html", [
+  [ "Rucksack (Python)", "index.html", [
     [ "Todo List", "todo.html", null ],
-    [ "Bug List", "bug.html", null ],
-    [ "Namespaces", null, [
-      [ "Namespace List", "namespaces.html", "namespaces" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ]
-      ] ]
-    ] ],
-    [ "Data Structures", "annotated.html", [
-      [ "Data Structures", "annotated.html", "annotated_dup" ],
-      [ "Data Structure Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Data Fields", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ]
-      ] ]
-    ] ],
     [ "Files", null, [
       [ "File List", "files.html", "files" ]
     ] ]
@@ -27,7 +10,7 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-".html"
+"files.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
