de3ee843474293d0a8ac479198394caa204765b4
Chris <cwunderlich@almandor.de>
2016-06-16 21:24:36 +0200

    Added Comments for Doxygen

--- src/start.py
+++ src/start.py
@@ -1,10 +1,32 @@
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+'''
+\file start.py
+\brief Rucksackverwaltung
+
+Rucksackverwaltung mit Shopsystem
+
+\date (C) 2015
+\author Christian Wunderlich
+\email cw@almandor.de
+
+\todo Modul: alles
+
+'''
 
 import Tkinter as tk
 import os.path
 import time
 
 class Mainscreen(object):
+
+    '''
+    Creates the Mainwindow where you can Create a new Group, load a Group or continue the last one
+
+    \todo Fill the buttons with function
+    '''
+
+
     def __init__(self, master):
         self.master = master
         self.frame_main = tk.Frame(self.master, height = 200, width = 600)
@@ -25,8 +47,10 @@ class Mainscreen(object):
         self.button_quit.grid(row = 5, column = 0, pady = 10)
         
     def __create_new_group(self):
-        # WIP
-        print("WIP! New")
+        '''
+        Closes the Mainwindow and starts the New Group Class
+        '''
+
         self.master.destroy()
         root = tk.Tk()
         app = New_Group(root)
@@ -34,43 +58,56 @@ class Mainscreen(object):
         root.mainloop()
 
     def __load_group(self):
-        # WIP
+        '''
+        Closes the Mainwindow and starts the Load Group Class
+
+        \todo Everything
+        '''
         print("WIP! Load")
 
 #
     def __continue_last_group(self):
-        # WIP
+        '''
+        Closes the Mainwindow and starts the Continue Last Group Class
+
+        \todo Everything
+        '''
         print("WIP!")
 
 
 class New_Group(object):
-        def __init__(self, master):
-            self.master = master
-            self.frame_main = tk.Frame(self.master, height = 600, width = 800)
-            self.frame_main.grid(row = 0, column = 0)
-            self.label1 = tk.Label(self.frame_main, text = "Rucksack und Inventar Management", font = ("Helvetica", 16))
-            self.label1.grid(row = 0, column = 0, padx = 10, columnspan = 4)
-            self.label_name = tk.Label(self.frame_main, text = "Name", font = ("Times", 8))
-            self.label_name.grid(row = 1, column = 0, pady = (10, 2))
-            self.entry_name = tk.Entry(self.frame_main)
-            self.entry_name.grid(row = 2, column = 0, padx = (10, 0))
-            self.label_weight = tk.Label(self.frame_main, text = "MaxGewicht", font = ("Times", 8))
-            self.label_weight.grid(row = 1, column = 1, pady = (10, 2))
-            self.entry_weight = tk.Entry(self.frame_main)
-            self.entry_weight.grid(row = 2, column = 1, padx = 0)
-            self.label_gold = tk.Label(self.frame_main, text = "Gold", font = ("Times", 8))
-            self.label_gold.grid(row = 1, column = 2, pady = (10, 2))
-            self.entry_gold = tk.Entry(self.frame_main)
-            self.entry_gold.grid(row = 2, column = 2, padx = 0)
-            self.label_player = tk.Label(self.frame_main, text = "Spielername", font = ("Times", 8))
-            self.label_player.grid(row = 1, column = 3, pady = (10, 2))
-            self.entry_player = tk.Entry(self.frame_main)
-            self.entry_player.grid(row = 2, column = 3, padx = (0, 10))
-            self.button_entry = tk.Button(self.frame_main, text = "+", command = self.__add_data)
-            self.button_entry.grid(row = 2, column = 4, padx = 10)
-            
-        def __add_data(self):
-            print("WIP")
+    '''
+    Presents a Window where you can create the Group
+    Very work in progress
+
+    '''
+    def __init__(self, master):
+        self.master = master
+        self.frame_main = tk.Frame(self.master, height = 600, width = 800)
+        self.frame_main.grid(row = 0, column = 0)
+        self.label1 = tk.Label(self.frame_main, text = "Rucksack und Inventar Management", font = ("Helvetica", 16))
+        self.label1.grid(row = 0, column = 0, padx = 10, columnspan = 4)
+        self.label_name = tk.Label(self.frame_main, text = "Name", font = ("Times", 8))
+        self.label_name.grid(row = 1, column = 0, pady = (10, 2))
+        self.entry_name = tk.Entry(self.frame_main)
+        self.entry_name.grid(row = 2, column = 0, padx = (10, 0))
+        self.label_weight = tk.Label(self.frame_main, text = "MaxGewicht", font = ("Times", 8))
+        self.label_weight.grid(row = 1, column = 1, pady = (10, 2))
+        self.entry_weight = tk.Entry(self.frame_main)
+        self.entry_weight.grid(row = 2, column = 1, padx = 0)
+        self.label_gold = tk.Label(self.frame_main, text = "Gold", font = ("Times", 8))
+        self.label_gold.grid(row = 1, column = 2, pady = (10, 2))
+        self.entry_gold = tk.Entry(self.frame_main)
+        self.entry_gold.grid(row = 2, column = 2, padx = 0)
+        self.label_player = tk.Label(self.frame_main, text = "Spielername", font = ("Times", 8))
+        self.label_player.grid(row = 1, column = 3, pady = (10, 2))
+        self.entry_player = tk.Entry(self.frame_main)
+        self.entry_player.grid(row = 2, column = 3, padx = (0, 10))
+        self.button_entry = tk.Button(self.frame_main, text = "+", command = self.__add_data)
+        self.button_entry.grid(row = 2, column = 4, padx = 10)
+
+    def __add_data(self):
+        print("WIP")
 
 
 
