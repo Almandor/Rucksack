d896b388ccb81e53f538f10a6af832236b324135
Christian <cwunderlich@almandor.de>
2017-01-30 11:33:02 +0100

    Fensterbau abgeschlossen

--- Shop/src/testinit.py
+++ Shop/src/testinit.py
@@ -29,27 +29,55 @@ class Startwindow(object):
         self.frame_main.grid(row = 0, column = 0)
         
         self.label1 = tk.Label(self.frame_main, text = "Testtool um Shop zu testen", font = ("Helvetica", 16))
-        self.label1.grid(row = 0, column = 0, padx = 10)
+        self.label1.grid(row = 0, column = 0, padx = 10, sticky=tk.W)
         
-        self.frameplaceholder = tk.Frame(self.frame_main, height = 50, width = 600)
+        self.frameplaceholder = tk.Frame(self.frame_main, height = 50, width = 600,relief=tk.RIDGE)
         self.frameplaceholder.grid(row = 1, column = 0, columnspan = 4)
         
         self.labelname = tk.Label(self.frame_main, text = "Name: ")
-        self.labelname.grid(row = 2,column = 0,padx = (0,10)) # Falsche Spalte
+        self.labelname.grid(row = 2,column = 0, sticky=tk.W, padx=(10,0))
 
+        self.entryname = tk.Entry(self.frame_main, textvariable=name)
+        self.entryname.config(width = 40)
+        self.entryname.grid(row = 2, column = 1, padx=(0,10))
+        
+        self.labelgold = tk.Label(self.frame_main, text = "Gold: ")
+        self.labelgold.grid(row = 3,column = 0, sticky=tk.W, padx=(10,0), pady=(5,0))
 
-#         self.button_new = tk.Button(self.frame_main, text = "Neue Gruppe anlegen", command = self.__create_new_group, height = 3, width = 30)
-#         self.button_new.grid(row = 1, column = 0, pady = 10)
-#         
-#         self.button_load = tk.Button(self.frame_main, text = "Vorhandene Gruppe laden", command = self.__load_group, height = 3, width = 30)
-#         self.button_load.grid(row = 2, column = 0, pady = 10)
-#         
-#         self.button_continue = tk.Button(self.frame_main, text = "Vorherige Gruppe fortführen", command = self.__continue_last_group, height = 3, width = 30)
-#         self.button_continue.grid(row = 3, column = 0, pady = (10, 30))
-# 
-#         self.button_quit = tk.Button(self.frame_main, text = "Beenden", command = master.destroy, height = 3, width = 30)
-#         self.button_quit.grid(row = 5, column = 0, pady = 10)   
+        self.entrygold = tk.Entry(self.frame_main, textvariable=gold)
+        self.entrygold.config(width = 40)
+        self.entrygold.grid(row = 3, column = 1, padx=(0,10), pady=(5,0))
+        
+        self.labelweight = tk.Label(self.frame_main, text = "Max Weight: ")
+        self.labelweight.grid(row = 4,column = 0, sticky=tk.W, padx=(10,0), pady=(5,0))
+
+        self.entryweight = tk.Entry(self.frame_main, textvariable=maxweight)
+        self.entryweight.config(width = 40)
+        self.entryweight.grid(row = 4, column = 1, padx=(0,10), pady=(5,0))
+        
+        self.labelcontainer = tk.Label(self.frame_main, text = "Prefilled Container: ")
+        self.labelcontainer.grid(row = 5,column = 0, sticky=tk.W, padx=(10,0), pady=(5,0))
+
+        self.checkcontainer = tk.Checkbutton(self.frame_main)
+        self.checkcontainer.grid(row = 5, column = 1, padx=(0,10), pady=(5,0), sticky=tk.W)
+
+        self.button_start = tk.Button(self.frame_main, text = "Start", command = self.begin(), height = 3, width = 30, foreground='green')
+        self.button_start.grid(row = 6, column = 0, pady = 10)   
+
+        self.button_quit = tk.Button(self.frame_main, text = "Beenden", command = master.destroy, height = 3, width = 30, foreground='red')
+        self.button_quit.grid(row = 6, column = 1, pady = 10)
+        
+        self.prefill(self.entryname, self.entrygold,self.entryweight,self.checkcontainer)
+        
+    def prefill(self,name,gold,weight,container):
+        name.insert(0, "Max Tester")
+        gold.insert(0, 100000)
+        weight.insert(0, 120)
+           
     
+    def begin(self):
+        pass
+
 
 def center(toplevel):
     toplevel.update_idletasks()
@@ -70,4 +98,8 @@ def main():
 
 
 if __name__ == '__main__':
+    name = ""
+    gold = "0"
+    maxweight = ""
+    container = []
     main()
