f7b052bb736cd8bff1f6c808641ed99fb5a9ced8
Christian Wunderlich <cwunderlich@almandor.de>
2017-09-21 15:39:26 +0200

    Prog1

---
+++ Prog1/prog1.py
@@ -0,0 +1,35 @@
+from Tkinter import *
+import ConfigParser
+import os
+
+configfile = 'prog1.cfg'
+
+def readConfig(configfile):
+    # Checking for Config File
+    if os.path.isfile(configfile):
+        config = ConfigParser.ConfigParser()
+        config.read(configfile)
+        try:
+            dataDirectory = config.get("1",'DataDirectory')
+        except:
+            print("DataDirectory not configured. Using defaults")
+            dataDirectory = "./Data"
+    else:
+        print("Config file not found. Using defaults")
+        dataDirectory = "./Data"
+
+    return(dataDirectory)
+
+def listCategories(dataDirectory):
+    print(os.listdir(dataDirectory))
+
+
+readConfig(configfile)
+#===============================================================================
+# root = Tk()
+# 
+# labelMain = Label(root, text="Get Inventory")
+# labelMain.grid()
+# 
+# root.mainloop()
+#===============================================================================
